fae8de3a585d360536933df4d8e9e7386b505f8f
Redwan Ahmed <122109755+redwan-ai@users.noreply.github.com>
2023-01-29 23:13:18 +0600

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,13 @@
+const navToggle = document.querySelector('.nav-toggle');
+navToggle.addEventListener('click',navTogo);
+
+function navTogo(){
+    const nav = document.querySelector('.nav')
+    nav.classlist.toggle('open');
+if(nav.classList.contains('open')){
+    nav.style.maxHeight = nav.scrollHeight + 'px';
+}
+else{
+    nav.removeAttribute('style')
+}
+}
